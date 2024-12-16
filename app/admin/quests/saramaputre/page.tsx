b35f4d35816b558c411e83643fce8691eb724104
VV17CH3R <vvitcherv@gmail.com>
2024-12-16 22:30:49 +0300

    com v.2 16.12.24

--- app/admin/quests/saramaputre/page.tsx
+++ app/admin/quests/saramaputre/page.tsx
@@ -2,12 +2,85 @@ import { cn } from "@/lib/utils";
 import { Axe, Backpack, Book, Leaf, ScrollText } from "lucide-react";
 import Link from "next/link";
 
-
-
 export default function QuestPg() {
-    return (
-        <div className="flex justify-center  items-center ">
-            Srama Putre Qv
-        </div>  
-    )
+  return (
+    <div className="flex items-center  justify-center">
+      <div className="flex mt-4 justify-center flex-col w-auto items-center ">
+
+      <br></br><span className="font-extrabold text-primary text-xl"> Нага Маясома  fnd/640 </span>   
+      
+      <br></br><span className="font-extrabold text-primary text-xl"> царь Джанамеджая  ltc/52 </span>
+
+        <br></br><span className="font-extrabold text-primary text-xl"> Храм Триумфа Панду  mkc/306 </span>
+
+        <br></br><span className="font-extrabold text-primary text-xl"> Братья Джанамеджаи plist/ORI-130 </span>
+        <br></br><span className="font-extrabold text-primary text-xl"> Сарама  mh3/22 </span>
+        <br></br><span className="font-extrabold text-primary text-xl"> Сын Сарамы  mh21/2 </span>
+
+        <br></br><span className="font-extrabold text-primary text-xl"> Затопленная Роща  dsc/276 </span>
+
+        <br></br><span className="font-extrabold text-primary text-xl"> Недоразвитые  hbg/198 </span>
+
+        <br></br><span className="font-extrabold text-primary text-xl"> Минари Двуглавый  plist/CLB-225 </span>
+
+
+        <br></br><span className="font-extrabold text-primary text-xl"> Шруташравас  j25/260 </span>
+
+        <br></br><span className="font-extrabold text-primary text-xl"> СомаШравас  fnd/649 </span>
+
+        <br></br><span className="font-extrabold text-primary text-xl"> Камней Душ  fnd/133 </span>
+
+
+
+        <br></br><span className="font-extrabold text-primary text-xl"> ТакшаШира  dsc/267 </span>
+
+        <br></br><span className="font-extrabold text-primary text-xl"> Палладины  clu/55 </span>
+        <br></br><span className="font-extrabold text-primary text-xl"> Големы Элит   rvr/7 </span>
+        <br></br><span className="font-extrabold text-primary text-xl"> Ловцы   clu/64 </span>
+        <br></br><span className="font-extrabold text-primary text-xl"> Башенный Патруль   j25/236 </span>
+        <br></br><span className="font-extrabold text-primary text-xl"> Говорящие с серафимами   clu/79 </span>
+        <br></br><span className="font-extrabold text-primary text-xl"> Ассасины   clu/123 </span>
+        <br></br><span className="font-extrabold text-primary text-xl"> Молодая гвардия   clu/192 </span>
+        <br></br><span className="font-extrabold text-primary text-xl"> Духо-Воды   plis/RNA-16 </span>
+
+
+        <br></br><span className="font-extrabold text-primary text-xl">  Маршал Варис   j25/266 </span>
+        <br></br><span className="font-extrabold text-primary text-xl">  Дэви Левия  clu/199 </span>
+         
+            <br></br>
+
+            <text className="w-96">
+            Мотив призыва МаяСомы : 
+         У Нас есть сын, н он не признал меня своей
+        женой. Всех демонов знают. Ты новая в служении мироходка... ВЫ полни и я
+        дарую тебе Дар, Осквернять любую воду ядом. В твоем мире любят принимать
+        малые дозы яда и отправляться в состояние Призраков. Я дараю тебе Дар
+        над этими веществами. Я их повелительница, я их совершенно освоила. И
+        так оскверни его Жертвоприношение... Оно почти завершено и будет идти
+        еще 3 дня.
+        <br></br> Решение Квеста : <br></br>
+         Братьев Споить... Избили собаку... Донести до Сарамы... На
+        тебе клеймо неудачи .. Сарама Прокляла... Что бы ты не делал будет
+        неудачно....
+        <br></br>
+        
+         Это проклятие может снять только сын наги... Они хотят
+        снять проклятие! Они закрылись в ТакшаШире... Верни мне моего сына!
+        Собрать отряд, взять крепость... 7 дней будет идти обряд очищения.
+        Надень это и попроси его никогда более не покидать мать ... Он не сможет
+        отказать Брахману... В древности он дал обет... 
+        <br></br> Осда города... <br></br>
+
+        Мотивы: Джанамеджаи:
+        "Моя Ягья провалилось, люди будут страдать без достойного царя... Нужно найти способ снять проклятие Сарамы.."<br></br>
+        Решение: Шрута Шравас даст побочку... Расположение Мудреца... Мой сын может снять он славен подвигами... Но Мать Жадно стережет его. Передай ему послание, что я зову его. 
+        Он в дал обет. От того он пойдет с Вами. Лишь скажите ему. 
+
+        <br></br> При начале ягьи МаяСома Насылает на город чуму... Нужно спасит Город.
+
+            </text>
+         
+      </div>
+    </div>
+  );
 }
