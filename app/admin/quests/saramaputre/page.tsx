8ca7cc8f8372f58b3564428f8c1f8cf579d13a02
VV17CH3R <vvitcherv@gmail.com>
2024-12-17 09:02:13 +0300

    17.12.24 v.1.2

--- app/admin/quests/saramaputre/page.tsx
+++ app/admin/quests/saramaputre/page.tsx
@@ -6,86 +6,55 @@ export default function SaramaPutre() {
   return (
     <div className="flex items-center  justify-center">
       <div className="flex mt-4 justify-center flex-col w-auto items-center ">
-        <text className="font-extrabold text-primary text-xl">
-          {
-            `
-            <br></br><span className="font-extrabold text-primary text-xl"> Нага Маясома  fnd/640 </span>   
+        <text className=" w-3/4 text-primary text-lg">
+          {`
+             --- Нага Маясома  fnd/640 ---
       
-            <br></br><span className="font-extrabold text-primary text-xl"> царь Джанамеджая  ltc/52 </span>
+             --- царь Джанамеджая  ltc/52  ---
       
-              <br></br><span className="font-extrabold text-primary text-xl"> Храм Триумфа Панду  mkc/306 </span>
+             ---  Храм Триумфа Панду  mkc/306  ---
       
-              <br></br><span className="font-extrabold text-primary text-xl"> Братья Джанамеджаи plist/ORI-130 </span>
-              <br></br><span className="font-extrabold text-primary text-xl"> Сарама  mh3/22 </span>
-              <br></br><span className="font-extrabold text-primary text-xl"> Сын Сарамы  mh21/2 </span>
+             --- Братья Джанамеджаи plist/ORI-130  ---
+             --- Сарама  mh3/22  ---
+             --- Сын Сарамы  mh21/2  ---
       
-              <br></br><span className="font-extrabold text-primary text-xl"> Затопленная Роща  dsc/276 </span>
+             --- Затопленная Роща  dsc/276  ---
       
-              <br></br><span className="font-extrabold text-primary text-xl"> Недоразвитые  hbg/198 </span>
+             --- Недоразвитые  hbg/198  ---
       
-              <br></br><span className="font-extrabold text-primary text-xl"> Минари Двуглавый  plist/CLB-225 </span>
-      
-      
-              <br></br><span className="font-extrabold text-primary text-xl"> Шруташравас  j25/260 </span>
-      
-              <br></br><span className="font-extrabold text-primary text-xl"> СомаШравас  fnd/649 </span>
-      
-              <br></br><span className="font-extrabold text-primary text-xl"> Камней Душ  fnd/133 </span>
+             ---  Минари Двуглавый  plist/CLB-225  ---
       
       
+             --- Шруташравас  j25/260  ---
       
-              <br></br><span className="font-extrabold text-primary text-xl"> ТакшаШира  dsc/267 </span>
+             ---  СомаШравас  fnd/649  ---
       
-              <br></br><span className="font-extrabold text-primary text-xl"> Палладины  clu/55 </span>
-              <br></br><span className="font-extrabold text-primary text-xl"> Големы Элит   rvr/7 </span>
-              <br></br><span className="font-extrabold text-primary text-xl"> Ловцы   clu/64 </span>
-              <br></br><span className="font-extrabold text-primary text-xl"> Башенный Патруль   j25/236 </span>
-              <br></br><span className="font-extrabold text-primary text-xl"> Говорящие с серафимами   clu/79 </span>
-              <br></br><span className="font-extrabold text-primary text-xl"> Ассасины   clu/123 </span>
-              <br></br><span className="font-extrabold text-primary text-xl"> Молодая гвардия   clu/192 </span>
-              <br></br><span className="font-extrabold text-primary text-xl"> Духо-Воды   plis/RNA-16 </span>
+             ---  Камней Душ  fnd/133  ---
       
       
-              <br></br><span className="font-extrabold text-primary text-xl">  Маршал Варис   j25/266 </span>
-              <br></br><span className="font-extrabold text-primary text-xl">  Дэви Левия  clu/199 </span>
-              </text>
+      
+             --- ТакшаШира  dsc/267  ---
+      
+             --- Палладины  clu/55  ---
+             ---  Големы Элит   rvr/7  ---
+             ---  Ловцы   clu/64  ---
+             ---  Башенный Патруль   j25/236  ---
+             ---  Говорящие с серафимами   clu/79  ---
+             --- Ассасины   clu/123  ---
+             ---  Молодая гвардия   clu/192  ---
+             --- Духо-Воды   plis/RNA-16  ---
+      
+      
+             ---  Маршал Варис   j25/266  ---
+             ---  Дэви Левия  clu/199  ---
+             
 
  
          
-            <br></br>
+             
 
-            <text className="w-96">
-            Мотив призыва МаяСомы : 
-         У Нас есть сын, н он не признал меня своей
-        женой. Всех демонов знают. Ты новая в служении мироходка... ВЫ полни и я
-        дарую тебе Дар, Осквернять любую воду ядом. В твоем мире любят принимать
-        малые дозы яда и отправляться в состояние Призраков. Я дараю тебе Дар
-        над этими веществами. Я их повелительница, я их совершенно освоила. И
-        так оскверни его Жертвоприношение... Оно почти завершено и будет идти
-        еще 3 дня.
-        <br></br> Решение Квеста : <br></br>
-         Братьев Споить... Избили собаку... Донести до Сарамы... На
-        тебе клеймо неудачи .. Сарама Прокляла... Что бы ты не делал будет
-        неудачно....
-        <br></br>
-        
-         Это проклятие может снять только сын наги... Они хотят
-        снять проклятие! Они закрылись в ТакшаШире... Верни мне моего сына!
-        Собрать отряд, взять крепость... 7 дней будет идти обряд очищения.
-        Надень это и попроси его никогда более не покидать мать ... Он не сможет
-        отказать Брахману... В древности он дал обет... 
-        <br></br> Осда города... <br></br>
-
-        Мотивы: Джанамеджаи:
-        "Моя Ягья провалилось, люди будут страдать без достойного царя... Нужно найти способ снять проклятие Сарамы.."<br></br>
-        Решение: Шрута Шравас даст побочку... Расположение Мудреца... Мой сын может снять он славен подвигами... Но Мать Жадно стережет его. Передай ему послание, что я зову его. 
-        Он в дал обет. От того он пойдет с Вами. Лишь скажите ему. 
-
-        <br></br> При начале ягьи МаяСома Насылает на город чуму... Нужно спасит Город.
-
-            </text>
-            `
-          }
+            
+            `}
         </text>
       </div>
     </div>
